bc0f09067308028dcc8b81d3d7bc7f5701a42a02
Danish Haroon <danishmohammedharoon@gmail.com>
2025-03-20 08:34:54 -0230

    removes the redundant app.listen to be replaced with server.listen

--- server.js
+++ server.js
@@ -77,11 +77,6 @@ io.on("connection", (socket) => {
 });
  
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+server.listen(PORT, () => {
+ console.log(`Server is running on port ${PORT}`);
 });
-
-// trial for web socket
-//server.listen(PORT, () => {
-//  console.log(`Server is running on port ${PORT}`);
-//});
