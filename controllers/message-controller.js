4cb7c4e07b06af50d6c3fb9084b0bba3ea052967
Danish Haroon <danishmohammedharoon@gmail.com>
2025-03-20 10:57:21 -0230

    remving unnecessary comments from message controller

--- controllers/message-controller.js
+++ controllers/message-controller.js
@@ -1,32 +1,5 @@
 import knex from "../db/knex.js";
 
-// Send a new message using Knex
-// export const sendMessage = async (req, res) => {
-//   const { from, to, message } = req.body;
-
-//   if (!from || !to || !message) {
-//     return res
-//       .status(400)
-//       .json({ message: "from, to, and message fields are required." });
-//   }
-
-//   try {
-//     const [id] = await knex("messages").insert({
-//       from,
-//       to,
-//       message,
-//       timestamp: new Date().toISOString(),
-//     });
-//     const newMessage = await knex("messages").where({ id }).first();
-//     return res
-//       .status(201)
-//       .json({ message: "Message sent successfully", data: newMessage });
-//   } catch (error) {
-//     console.error("Error sending message:", error);
-//     return res.status(500).json({ message: "Internal Server Error" });
-//   }
-// };
-
 const formatDateTime = (isoString) =>
   isoString.replace("T", " ").substring(0, 19);
 
@@ -39,8 +12,8 @@ export const sendMessage = async (req, res) => {
 
   try {
     const [id] = await knex("messages").insert({
-      from: String(from), // Convert to string if needed
-      to: String(to), // Convert to string if needed
+      from: String(from),
+      to: String(to), 
       message,
       timestamp: formatDateTime(timestamp),
     });
